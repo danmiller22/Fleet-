39b25247c2586a1d6d6164342c3da0ff893a9303
danmiller22 <_EMAIL@.com>
2025-09-11 06:53:07 -0500

    fix(tailwind): восстановил стили на Pages
    - Перешёл на v4 синтаксис: @import tailwindcss в src/styles.css
    - tailwind.config.js переведён на ESM export

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: 'class',
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./index.html",
-    "./*.{js,jsx,html}",
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
